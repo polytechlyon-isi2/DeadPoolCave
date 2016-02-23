346048f8e385ddc74f54856988c7b38d6f881693
root <sam-j@hotmail.fr>
2016-02-23 20:14:51 +0100

    iteration 1 modification des fichiers creation de la base de donnee

--- db/structure.sql
+++ db/structure.sql
@@ -5,14 +5,32 @@ create table t_article (
 
 art_id integer not null primary key auto_increment,
 
+art_ref varchar(100),
+    
 art_title varchar(100) not null,
 
 art_content varchar(2000),
 
-art_author varchar(50) not null,
-
+art_editor varchar(20),
+    
 art_genre varchar(20),
+    
+art_series varchar(50),
     
 art_price integer 
 
-) engine=innodb character set utf8 collate utf8_unicode_ci;
+);
+
+drop table if exists t_author;
+
+
+create table t_author (
+
+    aut_id integer not null primary key auto_increment,
+    
+    aut_name varchar(50) not null,
+    
+    aut_firstname varchar(50)
+    
+    
+);
