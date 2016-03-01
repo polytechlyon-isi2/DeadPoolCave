160d637def690ad2c3e77763c6cae328643c2d4a
thethego <theo.goria@etu.univ-lyon1.fr>
2016-03-01 15:06:39 +0100

    Itération 7 : affichage des détails sur un article

--- app/app.php
+++ app/app.php
@@ -2,18 +2,24 @@
 
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
-use DeadPoolCave\DAO;
 
 // Register global error and exception handlers
 ErrorHandler::register();
 ExceptionHandler::register();
 
-// Register service providers.
+// Register service providers
 $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views'));
+    'twig.path' => __DIR__.'/../views',
+));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
-// Register services.
+// Register services
 $app['dao.article'] = $app->share(function ($app) {
     return new DeadPoolCave\DAO\ArticleDAO($app['db']);
+});
+$app['dao.comment'] = $app->share(function ($app) {
+    $commentDAO = new DeadPoolCave\DAO\CommentDAO($app['db']);
+    $commentDAO->setArticleDAO($app['dao.article']);
+    return $commentDAO;
 });
--- db/structure.sql
+++ db/structure.sql
@@ -1,36 +1,32 @@
+
 drop table if exists t_article;
-
-
-create table t_article (
-
-art_id integer not null primary key auto_increment,
-
-art_ref varchar(100),
-    
-art_title varchar(100) not null,
-
-art_content varchar(2000),
-
-art_editor varchar(20),
-    
-art_genre varchar(20),
-    
-art_series varchar(50),
-    
-art_price integer 
-
-);
-
+drop table if exists t_comment;
 drop table if exists t_author;
 
 
-create table t_author (
+create table t_article (
+art_id integer not null primary key auto_increment,
+art_ref varchar(100),
+art_title varchar(100) not null,
+art_content varchar(2000),
+art_editor varchar(20),
+art_genre varchar(20),
+art_series varchar(50),
+art_price integer 
+);
 
+
+create table t_comment (
+    com_id integer not null primary key auto_increment,
+    com_author varchar(100) not null,
+    com_content varchar(500) not null,
+    art_id integer not null,
+    constraint fk_com_art foreign key(art_id) references t_article(art_id)
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+
+create table t_author (
     aut_id integer not null primary key auto_increment,
-    
     aut_name varchar(50) not null,
-    
     aut_firstname varchar(50)
-    
-    
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
