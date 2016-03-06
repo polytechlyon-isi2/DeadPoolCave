db88bf1231043ba9d946814d4564bfaeeba0b03b
thethego <theo.goria@etu.univ-lyon1.fr>
2016-03-06 16:02:26 +0100

    Itération 12 : ajout d'une API JSON

--- app/app.php
+++ app/app.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
+use Symfony\Component\HttpFoundation\Request;
 
 // Register global error and exception handlers
 ErrorHandler::register();
@@ -82,4 +83,12 @@ $app->error(function (\Exception $e, $code) use ($app) {
             $message = "Something went wrong.";
     }
     return $app['twig']->render('error.html.twig', array('message' => $message));
+});
+
+// Register JSON data decoder for JSON requests
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
 });
