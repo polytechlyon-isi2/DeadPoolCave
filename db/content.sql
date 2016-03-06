0a998030e6f424e8ee5849d8983e21c61debb5cf
thethego <theo.goria@etu.univ-lyon1.fr>
2016-03-06 14:57:11 +0100

    Itération 10 : back-office d'administration & favicon

--- db/content.sql
+++ db/content.sql
@@ -8,5 +8,9 @@ insert into t_user values
 insert into t_user values
 (2, 'JaneDoe', 'EfakNLxyhHy2hVJlxDmVNl1pmgjUZl99gtQ+V3mxSeD8IjeZJ8abnFIpw9QNahwAlEaXBiQUBLXKWRzOmSr8HQ==', 'dhMTBkzwDKxnD;4KNs,4ENy', 'ROLE_USER');
 
+/* raw password is '@dm1n' */
+insert into t_user values
+(3, 'admin', 'gqeuP4YJ8hU3ZqGwGikB6+rcZBqefVy+7hTLQkOD+jwVkp4fkS7/gr1rAQfn9VUKWc7bvOD7OsXrQQN5KGHbfg==', 'EDDsl&fBCJB|a5XUtAlnQN8', 'ROLE_ADMIN');
+
 insert into t_comment values
 (1, 'Great! Keep up the good work.', 1, 1);
